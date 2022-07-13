709d5adbd0ae5b4e9e08fa66d5e8cab73fb443c8
DTT-DennisM <109212306+DTT-DennisM@users.noreply.github.com>
2022-07-13 14:50:28 +0200

    Create getBasicData.py

---
+++ COMPANION_COMPUTER/mavlink/getBasicData.py
@@ -0,0 +1,17 @@
+from pymavlink import mavutil
+import time
+
+px4 = mavutil.mavlink_connection("/dev/ttyACM0", baud=9600)
+
+print("Waiting for heartbeat")
+
+px4.wait_heartbeat()
+
+print("Received heartbeat from px4!!")
+
+while True:
+	try:
+		print(px4.recv_match().to_dict())
+	except:
+		pass
+	time.sleep(1.0)
