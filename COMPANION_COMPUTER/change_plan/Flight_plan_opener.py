1a107b9913bf1bbe7b511e105b8e5bfae1648c7f
DTT-DennisM <109212306+DTT-DennisM@users.noreply.github.com>
2022-07-14 12:00:59 +0200

    Update Flight_plan_opener.py

--- COMPANION_COMPUTER/change_plan/Flight_plan_opener.py
+++ COMPANION_COMPUTER/change_plan/Flight_plan_opener.py
@@ -18,11 +18,11 @@ from tkinter.messagebox import showinfo
 from jsontest2 import get_latlon, mv_waypoints, rot_waypoints
 import clipboard
 
+# Create window
 root = tk.Tk()
 root.title('QGC mission changer')
 root.resizable(False,False)
 root.geometry('700x250')
-
 
 
 def select_file():
@@ -30,25 +30,30 @@ def select_file():
     global full_plan
     global file_path
 
+   # Which files can be seen in explorer
     filetypes = (
         ('plan files','*.plan'),
         ('All files','.*')
     )
-
+    
+    # Open file explorer
     file_path = fd.askopenfilename(
         title='Open a file',
         initialdir='/home/dennism/mavlink',
         filetypes=filetypes
     )
 
+    # Show which file is selected
     showinfo(
         title='Selected file',
         message=file_path
     )
-
+    
+    # Open file
     with open(file_path, 'r') as file_object:
         data = json.load(file_object)
-
+    
+    # Retrieve the data
     full_plan = data
     LLocation.configure(text="File path: "+file_path)
     LSave.configure(text="")
@@ -58,86 +63,93 @@ def mission_changer():
 
     global full_plan
     try:
+        # Get heading if filled in
         heading = int(THead.get("1.0","end-1c"))
     except:
         print("no heading")
     try:
+        # Get latitude
         lat = float(TLat.get("1.0","end-1c"))
     except:
         print("no lat")
     try:
+        # Get longitude
         lon = float(TLon.get("1.0","end-1c"))
     except:
         print("no lon")
     try:
         if heading > 0:
+            # Only rotate if heading has been filled in
             full_plan = rot_waypoints(file_path, heading)
+        # Move waypoints
         full_plan = mv_waypoints(file_path, lat, lon)
     except:
-        print("smth went wrong!")
+        print("something went wrong!")
     LSave.configure(text="")
+    # Successfully changed
     text_button.configure(text="Mission changed!")
-    #except:
-        #print("at least 1 field left blank")
-        #pass
-    #full_plan["mission"]["items"][1]["doJumpId"] = 10
 
 def paste():
-    #try:
-        TLat.config(state=tk.NORMAL)
-        TLon.config(state=tk.NORMAL)
-        TLat.delete('1.0', tk.END)
-        TLon.delete('1.0', tk.END)
-        pasted = clipboard.paste()
-        #print(pasted)
-        floats = pasted.split(", ")
-        TLat.insert(tk.END, floats[0])
-        TLon.insert(tk.END, floats[1])
-        print(floats[0])
-        print(floats[1])
-    #except:
-    #    print("nono")
-    #    pass
+    # Empty latitude and longitude text box
+    TLat.config(state=tk.NORMAL)
+    TLon.config(state=tk.NORMAL)
+    TLat.delete('1.0', tk.END)
+    TLon.delete('1.0', tk.END)
+    
+    # Get clipboard data
+    pasted = clipboard.paste()
+    
+    # Split latitude and longitude and fill them in
+    floats = pasted.split(", ")
+    TLat.insert(tk.END, floats[0])
+    TLon.insert(tk.END, floats[1])
 
 def from_url():
     try:
+        # Get google link
         s = clipboard.paste()
-        print(s)
         found = s.find("www.google.com/maps")
         if found != -1:
+            # Go to the location of the coordinates in link
             print("found! : " + s)
             found = s.find('@')
             s = s[found+1:]
-            print(s)
+            
+            # Get latitude value
             lat = s[:s.find(',')]
             s = s[s.find(',')+1:]
-            print(s)
+            
+            # Get longitude value
             lon = s[:s.find(',')]
+            
+            # Empty the latitude and longitude text box
             TLat.config(state=tk.NORMAL)
             TLon.config(state=tk.NORMAL)
             TLat.delete('1.0', tk.END)
             TLon.delete('1.0', tk.END)
+            
+            # Fill in latitude and longitude
             TLat.insert(tk.END, lat)
             TLon.insert(tk.END, lon)
-            print(lat)
-            print(lon)
         else:
             print("no correct google url found!")
     except:
-        print("smth went wrong hiero")
-    print("from url!")
+        print("something went wrong")
 
 def file_save():
+    # Open explorer to save file
     save_name=fd.asksaveasfile(mode='w',defaultextension=".plan")
     if save_name is None:
         return
+     
+    # Save file
     file2save = json.dumps(full_plan)
     save_name.write(file2save)
     save_name.close
     LSave.configure(text="Successfully saved!")
     text_button.configure(text="Mission change")
 
-# Button
+# Open file button
 open_button = ttk.Button(
     root,
     text="Open a file",
@@ -146,7 +158,7 @@ open_button = ttk.Button(
 
 open_button.grid(column=0,row=1,sticky='w',padx=10,pady=10)
 
-# Button 2
+# Mission change button
 text_button = ttk.Button(
     root,
     text="Mission change",
@@ -155,7 +167,7 @@ text_button = ttk.Button(
 
 text_button.grid(column=0,row=2,sticky='w',padx=10,pady=10)
 
-# Button 3
+# Paste button
 paste_button = ttk.Button(
     root,
     text="Paste",
@@ -164,7 +176,7 @@ paste_button = ttk.Button(
 
 paste_button.grid(column=7,row=2,sticky='w',padx=10,pady=10)
 
-# Button 5
+# Import from URL button
 from_url_button = ttk.Button(
     root,
     text="Import from URL",
@@ -173,7 +185,7 @@ from_url_button = ttk.Button(
 
 from_url_button.grid(column=0,row=3,sticky='w',padx=10,pady=10)
 
-# Button 4
+# Save plan button
 save_button = ttk.Button(
     root,
     text="Save plan",
@@ -182,7 +194,7 @@ save_button = ttk.Button(
 
 save_button.grid(column=0,row=4,sticky='w',padx=10,pady=10)
 
-
+# All the labels and text
 LLocation = tk.Label(root, text='File path: ')
 LLocation.grid(column=1,row=1,sticky='w',padx=10,pady=1,columnspan=6)
 
@@ -191,15 +203,7 @@ LHead.grid(column=1,row=2,sticky='w',padx=10,pady=10,columnspan=1)
 THead = tk.Text(root, width=5, height=1)
 THead.grid(column=2,row=2,sticky='e',padx=10,pady=10,columnspan=1)
 
-#floats = list(map(float, clipboard.paste().split()))
-#print(floats)
 THead.bind('<Control-v>', lambda _:'break')
-"""
-EHead = tk.Entry(root, exportselection=0)
-EHead.insert(0, "Type words here!")
-EHead.bind('<Control-v>', lambda _:'break')
-EHead.grid(column=2,row=2,sticky='e',padx=10,pady=10,columnspan=1)
-"""
 
 LLat = tk.Label(root, text='lat: ')
 LLat.grid(column=3,row=2,sticky='w',padx=10,pady=10,columnspan=1)
@@ -213,22 +217,5 @@ TLon.grid(column=6,row=2,sticky='e',padx=10,pady=10,columnspan=1)
 
 LSave = tk.Label(root, text='')
 LSave.grid(column=1,row=3,sticky='w',padx=10,pady=10,columnspan=6)
-#file_path = fd.askopenfilename()
-
-'''
-print(file_path)
-
-
-#files = 'Transition_test.plan'
-
-with open(file_path, 'r') as file_object:
-    data = json.load(file_object)
-    print(data["mission"]["items"][0]["params"][4:6])
-
-
-# replace a value
-data["mission"]["items"][0]["params"][4:6] = [52, 6]
-print(data["mission"]["items"][0]["params"][4:6])
-'''
 
 root.mainloop()
